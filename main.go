41a63da98bdf45caca3183224315e9cdbed61dc0
Shashank Shekhar <shashank.shekhar@zop.dev>
2024-11-24 05:38:33 +0530

    Add envs

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"net/http"
+	"strings"
+
 	"gofr.dev/pkg/gofr"
 	vertex_ai "gofr.dev/pkg/gofr/ai/vertex-ai"
+	gofrHTTPPkg "gofr.dev/pkg/gofr/http"
 )
+
+type ErrorSendMessage struct{}
+
+func (e ErrorSendMessage) Error() string {
+	return "failed to send message"
+}
+
+func (e ErrorSendMessage) StatusCode() int {
+	return http.StatusBadRequest
+}
 
 func main() {
 	app := gofr.New()
@@ -11,12 +25,13 @@ func main() {
 	creds := app.Config.Get("SVC_ACC_CREDS")
 
 	vertexAIConfigs := &vertex_ai.Configs{
-		ProjectID:   "endless-fire-437206-j7",
-		LocationID:  "us-central1",
-		APIEndpoint: "us-central1-aiplatform.googleapis.com",
-		Datastore:   "projects/endless-fire-437206-j7/locations/global/collections/default_collection/dataStores/gofr-datastore_1732298621027",
-		ModelID:     "gemini-1.5-pro-002",
-		Credentials: creds,
+		ProjectID:         app.Config.Get("SVC_ACC_PROJECT_ID"),
+		LocationID:        app.Config.Get("SVC_ACC_LOCATION_ID"),
+		APIEndpoint:       app.Config.Get("SVC_ACC_ENDPOINT"),
+		Datastore:         strings.Split(app.Config.Get("SVC_ACC_DATASTORE"), ","),
+		SystemInstruction: []string{app.Config.Get("SVC_ACC_SYSTEM_INSTRUCTION")},
+		ModelID:           app.Config.Get("SVC_ACC_MODEL_ID"),
+		Credentials:       creds,
 	}
 
 	vertexAIClient, err := vertex_ai.NewVertexAIClientWithKey(vertexAIConfigs)
@@ -34,7 +49,18 @@ func main() {
 			return nil, err
 		}
 
-		return c.VertexAI.SendMessageUsingSystemInstruction(prompt, []string{"use only one documentation i.e. using-http-server"})
+		if len(prompt) == 0 {
+			return nil, gofrHTTPPkg.ErrorMissingParam{Params: []string{"body"}}
+		}
+
+		resp, err := c.VertexAI.SendMessage(c, prompt)
+		if err != nil {
+			c.Logger.Errorf("failed to send message: %v", err)
+
+			return nil, ErrorSendMessage{}
+		}
+
+		return resp, nil
 	})
 
 	app.Run()
